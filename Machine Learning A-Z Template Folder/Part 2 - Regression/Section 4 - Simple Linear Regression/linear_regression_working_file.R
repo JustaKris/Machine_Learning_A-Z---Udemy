f4958a7d7e63ebdf3fe7d82259b13333312c3472
Kristiyan Bonev <k.s.bonev@gmail.com>
2019-09-13 17:11:01 +0300

    End of Simple linear regression

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/linear_regression_working_file.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/linear_regression_working_file.R
@@ -1,4 +1,4 @@
-# Data preprocessing
+# Simple Linear Regression
 
 # Importing the dataset
 dataset = read.csv('Salary_Data.csv')
@@ -8,10 +8,49 @@ dataset = read.csv('Salary_Data.csv')
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$YearsExperience, SplitRatio = 2/3)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature scaling
 # training_set[, 2:3] = scale(training_set[, 2:3])
 # test_set[, 2:3] = scale(test_set[, 2:3])
+
+# Fitting Simple Linear regression to the data set
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = training_set)
+
+# Predicting the Test set
+y_pred = predict(regressor, newdata = test_set)
+
+# Visialising the Training set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Training set)') +
+  xlab('Year of experience') +
+  ylab('Salary')
+
+# Visualizing the Test set results
+ggplot() +
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            colour = 'blue') +
+  ggtitle('Salary vs Experience (Test set)') +
+  xlab('Year of experience') +
+  ylab('Salary')
+
+
+
+
+
+
+
+
+
+
+
