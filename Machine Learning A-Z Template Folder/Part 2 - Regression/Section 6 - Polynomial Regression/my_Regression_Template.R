43676f5517fa1b912aacd93e5bb9bceddc4a4031
Kristiyan Bonev <k.s.bonev@gmail.com>
2020-01-30 17:00:52 +0200

    idk what i changed really

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/my_Regression_Template.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/my_Regression_Template.R
@@ -1,9 +1,5 @@
 # Regression Template
 
-# Polynaomial Regression
-
-# Data Preprocessing Template
-
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
@@ -20,37 +16,46 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Linear Regression to the dataset
-lin_reg = lm(formula = Salary ~ .,
-             data = dataset)
 
-# Fitting the Polynomial Regression to the dataset
+# Fitting the Regression model to the dataset
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
 dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ .,
               data = dataset)
 
-# Visualizing the Polynomial Regression results
+# Predict a new result
+y_pred = predict(regressor, data.frame(Level = 6.5))
+summary(y_pred)
+
+# Visualizing the Regression Model results
+# install.package(ggplot2)
 library(ggplot2)
 ggplot() + 
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = "red") +
-  geom_line(aes(dataset$Level, y = predict(poly_reg, newdata = dataset)),
+  geom_line(aes(dataset$Level, y = predict(regressor, newdata = dataset)),
             colour = "red") +
   ggtitle('Truth or Bluff (Plynomial Regression)') +
   xlab('Level') +
   ylab('Salary')
 
-# Predict a new result with Linear Regression
-y_pred = predict(lin_reg, data.frame(Level = 6.5))
 
-# Predict a new result with Polynomial Regression
-y_pred = predict(poly_reg, data.frame(Level = 6.5,
-                                      Level2 = 6.5^2,
-                                      Level3 = 6.5^3,
-                                      Level4 = 6.5^4))
-summary(y_pred)
+# Visualizing the Regression model results (x_grid for higher resolution and smoother curve)
+# install.package(ggplot2)
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = "red") +
+  geom_line(aes(x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = "blue") +
+  ggtitle('Truth or Bluff (Plynomial Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+
+
 
 
 
--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/my_file.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/my_file.R
@@ -23,6 +23,42 @@ lin_reg = lm(formula = Salary ~ .,
              data = dataset)
 
 # Fitting the Polynomial Regression to the dataset
-dataset$Level2 = dataset$Level ^ 2
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ .,
               data = dataset)
+
+# Visialising the Linear Regression results
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = "red") +
+  geom_line(aes(dataset$Level, y = predict(lin_reg, newdata = dataset)),
+            colour = "red") +
+  ggtitle('Truth or Bluff (Linear Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Visualizing the Polynomial Regression results
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = "red") +
+  geom_line(aes(dataset$Level, y = predict(poly_reg, newdata = dataset)),
+            colour = "red") +
+  ggtitle('Truth or Bluff (Plynomial Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Predict a new result with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+# Predict a new result with Polynomial Regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4))
+summary(y_pred)
+
+
+
